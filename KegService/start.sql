f07dd89870c08d75961818728d97b73ae5ac42c9
Bash Ash <bash@hubspot.com>
2013-10-17 00:09:44 -0400

    Sql to remember

---
+++ KegService/start.sql
@@ -0,0 +1,7 @@
+INSERT INTO banners (url) values ('shocktop_pumpkin.png');
+INSERT INTO banners (url) values ('pufo.jpg');
+INSERT INTO banners (url) values ('sams_boston.jpg');
+INSERT INTO banners (url) values ('sams_october.jpg');
+INSERT INTO banners (url) values ('sams_winter.jpg');
+
+INSERT INTO kegs (name, bannerId) VALUES ('Shock Top Pumpkin Wheat', 1);
