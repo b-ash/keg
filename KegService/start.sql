7f463db4c468ca797f4da949c2931a5b8107d846
Bash Ash <bash@hubspot.com>
2013-10-18 20:24:56 -0400

    Moar start stuffs

--- KegService/start.sql
+++ KegService/start.sql
@@ -1,3 +1,5 @@
+USE Kegums;
+
 INSERT INTO banners (url) values ('shocktop_pumpkin.png');
 INSERT INTO banners (url) values ('pufo.jpg');
 INSERT INTO banners (url) values ('sams_boston.jpg');
@@ -7,3 +9,7 @@ INSERT INTO banners (url) values ('sams_winter.jpg');
 INSERT INTO kegs (name, bannerId) VALUES ('Shock Top Pumpkin Wheat', 1);
 
 INSERT INTO temperature (degrees) VALUES (32);
+
+INSERT INTO pours (kegId, volume) VALUES (1, 13);
+INSERT INTO pours (kegId, volume) VALUES (1, 8);
+INSERT INTO pours (kegId, volume) VALUES (1, 9);
